7e4b867b9b3f5d499d1b091531c99d3111a78238
Ruslan Symonenko <symonenkors@gmail.com>
2022-12-15 20:12:01 +0200

    made file stucture

---
+++ index2.php
@@ -0,0 +1,4 @@
+<?php
+
+// $messageFromWebsite = $_GET['message'];
+// echo ($messageFromWebsite);
---
+++ js/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const textToSend = document.querySelector('.textToSend');
+//   const sendForm = document.querySelector('.sendForm');
+//   const userReply = document.querySelector('.user-reply');
+
+//   const req = new XMLHttpRequest();
+
+//   function getText () {
+//     return textToSend.value
+//   }
+
+//   function getServerResponse (data) {
+//     console.log(data);
+
+//     userReply.textContent = data;
+//   }
+
+//   function sendRequest (dataToSend, input) {
+//     req.onreadystatechange = function () {
+//       if(this.readyState == 4 && this.status == 200) {
+//         getServerResponse(this.responseText);
+//       }
+//     }
+
+//     req.open('GET', `http://contact-form.my/index2.php?${input}=${dataToSend}`);
+
+//     req.send();
+//   }
+
+//   sendForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     let text = getText();
+//     let inputName = textToSend.getAttribute('name')
+
+//     sendRequest(text, inputName);
+//   })
+// })
